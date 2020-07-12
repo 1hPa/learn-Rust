6f4dab8760b27697b913f0f08d0ffdc718dae341
Yu <tuba0927hy@gmail.com>
2020-07-12 16:54:01 +0900

    feat: add about mut-array copy

--- BasicKnowledge/slice/slice.rs
+++ BasicKnowledge/slice/slice.rs
@@ -5,4 +5,12 @@ fn main(){
     let a1 = a;
     println!("{:?}", a);
     println!("{:?}", a1);
+
+    let mut b = [0; 5];
+    b[0] = 10;
+    println!("{}", b[0]);
+    let mut b1 = b;
+    b1[0] = 20;
+    println!("{:?}", b);
+    println!("{:?}", b1);
 }
