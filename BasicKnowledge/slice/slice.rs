0f17b58cda81eb7beea377e5f754ddff6974916d
Yu <tuba0927hy@gmail.com>
2020-07-13 08:04:33 +0900

    style: add comment

--- BasicKnowledge/slice/slice.rs
+++ BasicKnowledge/slice/slice.rs
@@ -2,20 +2,20 @@ fn main(){
     let a = [1,2,3,4,5,6,7,8];
     println!("{}", a[0]);
     println!("{}", a.len());
-    let a1 = a;
+    let a1 = a;                 //基本的な配列のコピー
     println!("{:?}", a);
     println!("{:?}", a1);
 
     let mut b = [0; 5];
     b[0] = 10;
     println!("{}", b[0]);
-    let mut b1 = b;
+    let mut b1 = b;             //mutableも同様
     b1[0] = 20;
     println!("{:?}", b);
     println!("{:?}", b1);
 
-    let c = &a[2..5];
-    println!("{:?}", c);
+    let c = &a[2..5];           //imutableのスライスはいくつでも作れる
+    println!("{:?}", c);        //配列自体はコピーされない
     println!("{}", c.len());
 
     let d = &a[3..];
@@ -23,13 +23,13 @@ fn main(){
     println!("{}", d.len());
 
     {
-        let e = &mut b[..];
+        let e = &mut b[..];     //mutableな参照は１つのみ
         e[1] = 20;
         println!("{}", e[1]);
         /*compiler err
         b[1] = 30;
         */
     }
-    b[1] = 30;
+    b[1] = 30;                  //bにアクセス
     println!("{}", b[1]);
 }
