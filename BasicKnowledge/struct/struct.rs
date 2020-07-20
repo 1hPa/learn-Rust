c166d1634f731cd756cd19fa2af0f28866f7e6cf
Yu <tuba0927hy@gmail.com>
2020-07-20 23:46:36 +0900

    feat: add find the distance

--- BasicKnowledge/struct/struct.rs
+++ BasicKnowledge/struct/struct.rs
@@ -2,3 +2,11 @@
 struct Point3D{
     x: f64, y:f64, z:f64
 }
+
+// find th distance
+fn distance(p1: &Point3D, p2: &Point3D) -> f64{
+    let dx = p1.x - p2.x;
+    let dy = p1.y - p2.y;
+    let dz = p1.z - p2.z;
+    (dx*dx + dy*dy + dz*dz).sqrt();
+}
