e16a0f8991da4d1619ecd94f08340a12f3e6e4ee
Yu <tuba0927hy@gmail.com>
2020-08-03 23:34:17 +0900

    feat: add Two-dimensional point

--- BasicKnowledge/trait/main.rs
+++ BasicKnowledge/trait/main.rs
@@ -2,3 +2,14 @@
 trait Distance{
     fn distance (&self, p: &Self) -> f64;
 }
+
+// Two-dimensional point
+struct Point{
+    x: f64, y: f64
+}
+
+impl Point{
+    fn new(x1: f64, y1: f64) -> Point{
+        Point{x: x1, y: y1}
+    }
+}
