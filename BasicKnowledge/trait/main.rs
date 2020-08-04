fbe795af71d489f713d82546144c4e31cbc7d727
Yu <tuba0927hy@gmail.com>
2020-08-05 06:43:46 +0900

    feat: add Implemetation

--- BasicKnowledge/trait/main.rs
+++ BasicKnowledge/trait/main.rs
@@ -28,6 +28,21 @@ struct Point{
     x: f64, y: f64, z: f64
 }
 
+impl Point3D{
+    fn new(x1: f64, y1: f64, z1: f64) -> Point{
+        Point{x: x1, y: y1, z: z1}
+    }
+}
+
+// Implementation(three-dismensional)
+impl Distance for Point3D{
+    fn distance (&self, p: &Point) -> f64{
+        let dx = self.x - p.x;
+        let dy = self.y - p.y;
+        (dx * dx * dy * dy).sqrt()
+    }
+}
+
 fn main(){
     let p1 = Point::new(0.0,0.0);
     let p2 = Point::new(10.0, 10.0);
